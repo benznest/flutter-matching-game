11970ce6bb234a996e5292ebcc4281a79aad9710
benznest <benz7net@gmail.com>
2019-05-22 19:29:56 +0700

    first commit

---
+++ android/app/src/main/java/com/benzneststudios/flutter_match_animal_game/MainActivity.java
@@ -0,0 +1,13 @@
+package com.benzneststudios.flutter_match_animal_game;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
